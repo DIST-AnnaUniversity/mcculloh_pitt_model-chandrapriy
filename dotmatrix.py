3013b7a2a7a844f662f4174767d0293e70897d4d
chandrapriy <108045872+chandrapriy@users.noreply.github.com>
2022-06-26 18:11:38 +0530

    Update dotmatrix.py

--- dotmatrix.py
+++ dotmatrix.py
@@ -1,21 +1,10 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 6,
-   "id": "e3e27bd5",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
+
+   "outputs": 
       "[[11  1 10]\n",
       " [ 8  0  9]\n",
       " [ 6  5  5]]\n"
      ]
-    }
-   ],
+   
    "source": [
     "import numpy as np\n",
     "\n",
@@ -25,22 +14,13 @@
     "print(C)"
    ]
   },
-  {
-   "cell_type": "code",
-   "execution_count": 7,
-   "id": "f3585f51",
-   "metadata": {},
+  
    "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
       "[[12  1 10]\n",
       " [ 8 -2  9]\n",
       " [ 4  5  5]]\n"
      ]
-    }
-   ],
+    
    "source": [
     "import numpy as np\n",
     "A = np.array([[9,4,6], [5,-6,7],[3,2,1]])\n",
@@ -48,23 +28,13 @@
     "C = A + B     \n",
     "print(C)"
    ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 8,
-   "id": "de0a4fc2",
-   "metadata": {},
+  
    "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
       "[[ 18 -12  24]\n",
       " [ 15 -36  14]\n",
       " [  9   6   4]]\n"
      ]
-    }
-   ],
+    
    "source": [
     "import numpy as np\n",
     "A = np.array([[2,4,6], [5,-6,7],[3,2,1]])\n",
@@ -72,57 +42,5 @@
     "C = A * B     \n",
     "print(C)"
    ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 11,
-   "id": "d5a77338",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Dot Product  : \n",
-      " [[22 12]\n",
-      " [40 32]]\n",
-      "\n",
-      "Dot Product  : \n",
-      " [[22 32]\n",
-      " [15 32]]\n"
-     ]
-    }
-   ],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "a70370da",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.12"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+ 
+ 
